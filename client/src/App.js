9723f19336a41b6a4125b2e55843f59bee2335cf
HwangMinHyeok <aldur930@gmail.com>
2023-03-10 17:27:13 +0900

    client setting

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./styles/GlobalStyle.css";
+
+function App() {
+  return (
+    <div>
+      <h1>main024 ㅎㅇㅌ</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/reducers/reducer.js
@@ -0,0 +1,16 @@
+import { SET_CURRENT_SONG_URL } from "../actions/actions";
+import { initialState } from "./initialState";
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_CURRENT_SONG_URL:
+      return {
+        ...state,
+        currentSongURL: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
