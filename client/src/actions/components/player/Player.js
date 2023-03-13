d9114e4a22842ee5d50085de12c7846ca6f2e5aa
chanheek <toadfwe1@naver.com>
2023-03-13 17:29:17 +0900

    #23 docs: NowPlaying 생성

---
+++ client/src/actions/components/player/Player.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import { GoPlay } from 'react-icons/go';
+import { MdSkipNext } from 'react-icons/md';
+const PlayWarp = styled.div`
+  width: 654px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  background-color: blue;
+  margin-left: 4.2vw;
+`;
+const PlayerBtnContainer = styled.div`
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  color: var(--color7);
+  .PreBtn {
+    height: 30px;
+    width: 30px;
+    cursor: pointer;
+    transform: scaleX(-1);
+  }
+  .NextBtn {
+    height: 30px;
+    width: 30px;
+    cursor: pointer;
+  }
+  .StartBtn {
+    width: 50px;
+    height: 35px;
+    cursor: pointer;
+  }
+`;
+function Player() {
+    return (
+        <PlayWarp>
+            <PlayerBtnContainer>
+                <MdSkipNext className='PreBtn' /> <GoPlay className='StartBtn' /><MdSkipNext className='NextBtn' />
+            </PlayerBtnContainer>
+        </PlayWarp>
+    )
+}
+
+export default Player;
