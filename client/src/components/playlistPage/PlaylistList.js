b6a79b6eb59a40a13ef81501890a3c227f7e8bc2
HwangMinHyeok <aldur930@gmail.com>
2023-03-21 18:10:59 +0900

    #24 feat: redux 추가, 자잘한 스타일 변경

--- client/src/components/playlistPage/PlaylistList.js
+++ client/src/components/playlistPage/PlaylistList.js
@@ -1,32 +1,41 @@
-import { PlaylistListContainer } from '../../styles/playlist';
+import { useDispatch } from 'react-redux';
+
+import { setPlaylist, togglePlay } from '../../actions/actions';
+import {
+  PlaylistListContainerWrapper,
+  PlaylistListContainer,
+} from '../../styles/playlist';
 
 const PlaylistList = ({ playlist }) => {
-  const handleSongClick = (song) => {
-    const newURL = `https://www.youtube.com/watch?v=${song.youtubeKey}`;
-    window.open(newURL);
+  const dispatch = useDispatch();
+  const handleSongClick = (idx) => {
+    dispatch(setPlaylist(playlist.slice(idx)));
+    dispatch(togglePlay());
   };
   return (
-    <PlaylistListContainer>
-      <div className='row'>
-        <div className='num'>{`${playlist.length}곡`}</div>
-        <div className='title'>제목</div>
-        <div className='artist'>아티스트</div>
-        <div className='album'>앨범</div>
-        <div className='playtime'>{playlist.totalPlayTime}</div>
-      </div>
-      {playlist.map((song, idx) => (
-        <button
-          key={song.id}
-          className='row'
-          onClick={() => handleSongClick(song)}>
-          <div className='num'>{`${idx + 1}`}</div>
-          <div className='title'>{song.title}</div>
-          <div className='artist'>{song.artist}</div>
-          <div className='album'>{''}</div>
-          <div className='playtime'>{song.playtime}</div>
-        </button>
-      ))}
-    </PlaylistListContainer>
+    <PlaylistListContainerWrapper>
+      <PlaylistListContainer>
+        <div className='row'>
+          <div className='num'>{`${playlist.length}곡`}</div>
+          <div className='title'>제목</div>
+          <div className='artist'>아티스트</div>
+          <div className='album'>앨범</div>
+          <div className='playtime'>{playlist.totalPlayTime}</div>
+        </div>
+        {playlist.map((song, idx) => (
+          <button
+            key={song.id}
+            className='row'
+            onClick={() => handleSongClick(idx)}>
+            <div className='num'>{`${idx + 1}`}</div>
+            <div className='title'>{song.title}</div>
+            <div className='artist'>{song.artist}</div>
+            <div className='album'>{''}</div>
+            <div className='playtime'>{song.playtime}</div>
+          </button>
+        ))}
+      </PlaylistListContainer>
+    </PlaylistListContainerWrapper>
   );
 };
 
