69051e74514a4d173fc0ca14d5f597dec84ab994
yuwon707 <yuwon9207@gmail.com>
2023-03-27 12:30:22 +0900

    #112 Refactor: get-dummyData 임시 사용

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 import axios from 'axios';
 
@@ -8,167 +8,15 @@ import PlaylistThumbnail from '../components/mainPage/PlaylistThumbnail';
 import PlaylistTrendy from '../components/mainPage/PlaylistTrendy/PlaylistTrendy';
 import { MainContent, StyledSlider } from '../styles/main';
 
-// const sliderContents = [
-//   {
-//     sliderId: 1,
-//     sliderTitle: '지금 가장 인기 있는 리스트',
-//     sliderList: [0, 1, 2, 3, 4, 5],
-//   },
-//   {
-//     sliderId: 2,
-//     sliderTitle: '오늘의 추천 플레이리스트',
-//     sliderList: [1, 2, 3, 4, 5, 6],
-//   },
-//   {
-//     sliderId: 3,
-//     sliderTitle: '오늘 날씨에 딱 어울려요',
-//     sliderList: [2, 3, 4, 5, 6, 7],
-//   },
-//   {
-//     sliderId: 4,
-//     sliderTitle: '드라이브 필수 준비물',
-//     sliderList: [3, 4, 5, 6, 7, 8],
-//   },
-//   {
-//     sliderId: 5,
-//     sliderTitle: '노동에는 노동요',
-//     sliderList: [4, 5, 6, 7, 8, 9],
-//   },
-//   {
-//     sliderId: 6,
-//     sliderTitle: '지금 가장 인기 있는 리스트',
-//     sliderList: [0, 1, 2, 3, 4, 5],
-//   },
-//   {
-//     sliderId: 7,
-//     sliderTitle: '오늘의 추천 플레이리스트',
-//     sliderList: [1, 2, 3, 4, 5, 6],
-//   },
-//   {
-//     sliderId: 8,
-//     sliderTitle: '오늘 날씨에 딱 어울려요',
-//     sliderList: [2, 3, 4, 5, 6, 7],
-//   },
-//   {
-//     sliderId: 9,
-//     sliderTitle: '드라이브 필수 준비물',
-//     sliderList: [3, 4, 5, 6, 7, 8],
-//   },
-//   {
-//     sliderId: 10,
-//     sliderTitle: '노동에는 노동요',
-//     sliderList: [4, 5, 6, 7, 8, 9],
-//   },
-//   {
-//     sliderId: 11,
-//     sliderTitle: '지금 가장 인기 있는 리스트',
-//     sliderList: [0, 1, 2, 3, 4, 5],
-//   },
-//   {
-//     sliderId: 12,
-//     sliderTitle: '오늘의 추천 플레이리스트',
-//     sliderList: [1, 2, 3, 4, 5, 6],
-//   },
-//   {
-//     sliderId: 13,
-//     sliderTitle: '오늘 날씨에 딱 어울려요',
-//     sliderList: [2, 3, 4, 5, 6, 7],
-//   },
-//   {
-//     sliderId: 14,
-//     sliderTitle: '드라이브 필수 준비물',
-//     sliderList: [3, 4, 5, 6, 7, 8],
-//   },
-//   {
-//     sliderId: 15,
-//     sliderTitle: '노동에는 노동요',
-//     sliderList: [4, 5, 6, 7, 8, 9],
-//   },
-//   {
-//     sliderId: 16,
-//     sliderTitle: '지금 가장 인기 있는 리스트',
-//     sliderList: [0, 1, 2, 3, 4, 5],
-//   },
-//   {
-//     sliderId: 17,
-//     sliderTitle: '오늘의 추천 플레이리스트',
-//     sliderList: [1, 2, 3, 4, 5, 6],
-//   },
-//   {
-//     sliderId: 18,
-//     sliderTitle: '오늘 날씨에 딱 어울려요',
-//     sliderList: [2, 3, 4, 5, 6, 7],
-//   },
-//   {
-//     sliderId: 19,
-//     sliderTitle: '드라이브 필수 준비물',
-//     sliderList: [3, 4, 5, 6, 7, 8],
-//   },
-//   {
-//     sliderId: 20,
-//     sliderTitle: '노동에는 노동요',
-//     sliderList: [4, 5, 6, 7, 8, 9],
-//   },
-//   {
-//     sliderId: 21,
-//     sliderTitle: '지금 가장 인기 있는 리스트',
-//     sliderList: [0, 1, 2, 3, 4, 5],
-//   },
-//   {
-//     sliderId: 22,
-//     sliderTitle: '오늘의 추천 플레이리스트',
-//     sliderList: [1, 2, 3, 4, 5, 6],
-//   },
-//   {
-//     sliderId: 23,
-//     sliderTitle: '오늘 날씨에 딱 어울려요',
-//     sliderList: [2, 3, 4, 5, 6, 7],
-//   },
-//   {
-//     sliderId: 24,
-//     sliderTitle: '드라이브 필수 준비물',
-//     sliderList: [3, 4, 5, 6, 7, 8],
-//   },
-//   {
-//     sliderId: 25,
-//     sliderTitle: '노동에는 노동요',
-//     sliderList: [4, 5, 6, 7, 8, 9],
-//   },
-//   {
-//     sliderId: 26,
-//     sliderTitle: '지금 가장 인기 있는 리스트',
-//     sliderList: [0, 1, 2, 3, 4, 5],
-//   },
-//   {
-//     sliderId: 27,
-//     sliderTitle: '오늘의 추천 플레이리스트',
-//     sliderList: [1, 2, 3, 4, 5, 6],
-//   },
-//   {
-//     sliderId: 28,
-//     sliderTitle: '오늘 날씨에 딱 어울려요',
-//     sliderList: [2, 3, 4, 5, 6, 7],
-//   },
-//   {
-//     sliderId: 29,
-//     sliderTitle: '드라이브 필수 준비물',
-//     sliderList: [3, 4, 5, 6, 7, 8],
-//   },
-//   {
-//     sliderId: 30,
-//     sliderTitle: '노동에는 노동요',
-//     sliderList: [4, 5, 6, 7, 8, 9],
-//   },
-// ];
-
 const Main = () => {
   const [playlist, setPlaylist] = useState([]);
   //eslint-disable-next-line no-unused-vars
   const [currentIndex, setCurrentIndex] = useState(0);
   const [hasMore, setHasMore] = useState(true);
   const [displayCount, setDisplayCount] = useState(4);
+  const [sliderData, setSliderData] = useState([]);
 
-  const user = useSelector((state) => state.user);
+  // const user = useSelector((state) => state.user);
 
   const settings = {
     className: 'center',
@@ -181,55 +29,67 @@ const Main = () => {
     },
   };
 
-  // const fetchMoreData = useCallback(() => {
-  //   if (displayCount >= sliderContents.length) {
-  //     setHasMore(false);
-  //     return;
-  //   }
-  //   setDisplayCount((count) => count + 4);
-  // }, [sliderContents, displayCount]);
-
   const fetchMoreData = useCallback(() => {
-    if (displayCount >= setPlaylist.length) {
+    if (displayCount >= sliderData.length) {
       setHasMore(false);
       return;
     }
     setDisplayCount((count) => count + 4);
-  }, [setPlaylist, displayCount]);
-
-  // useEffect(() => {
-  //   const getPlaylist = async () => {
-  //     const response = await axios.get('http://localhost:3001/playlist');
-  //     setPlaylist(response.data);
-  //   };
-  //   getPlaylist();
-  // }, []);
+  }, [sliderData, displayCount]);
 
   useEffect(() => {
     const getPlaylist = async () => {
-      const response = await axios.get(
-        `http://15.165.199.44:8080/api/playlists?email=${user.email}`
-      );
+      const response = await axios.get('http://localhost:3001/playlist');
       setPlaylist(response.data);
     };
     getPlaylist();
   }, []);
 
-  // const visiblePlaylist = sliderContents.slice(0, displayCount);
+  useEffect(() => {
+    const getSlider = async () => {
+      const response = await axios.get('http://localhost:3001/slider');
+      setSliderData(response.data);
+    };
+    getSlider();
+  }, []);
 
-  // const visiblePlaylist = setPlaylist.slice(0, displayCount);
+  // useEffect(() => {
+  //   const getPlaylist = async () => {
+  //     const storedAccessToken = localStorage.getItem('accessToken');
+  //     if (storedAccessToken) {
+  //       try {
+  //         const response = await axios.get(
+  //           `http://15.165.199.44:8080/api/boards?page=1`,
+  //           {
+  //             headers: {
+  //               Authorization: `${storedAccessToken}`,
+  //               accept: 'application/json',
+  //             },
+  //           }
+  //         );
+  //         setPlaylist(response.data);
+  //         console.log(response.data);
+  //       } catch (e) {
+  //         console.log(`get playlist error`);
+  //       }
+  //     }
+  //   };
+  //   getPlaylist();
+  // }, []);
+
+  const visiblePlaylist = sliderData.slice(0, displayCount);
 
   return (
     <InfiniteScroll
-      dataLength={playlist.length}
+      dataLength={visiblePlaylist.length}
       next={fetchMoreData}
       hasMore={hasMore}
       loader={<h6>Loading...</h6>}
       style={{ overflow: 'visible' }}>
       <MainContent>
         {playlist.length > 0 && <PlaylistTrendy playlist={playlist[1]} />}
-        {playlist.length > 0 &&
-          playlist.map((el) => (
+        {visiblePlaylist.length > 0 &&
+          visiblePlaylist.map((el) => (
             <React.Fragment key={`section-${el.sliderId}`}>
               <div className='playlist-name' key={`title-${el.sliderId}`}>
                 {el.sliderTitle}
@@ -238,9 +98,9 @@ const Main = () => {
                 key={`slider-${el.sliderId}`}
                 {...settings}
                 afterChange={(idx) => setCurrentIndex(idx)}>
-                {playlist &&
+                {el.sliderList &&
                   el.sliderList.map((idx) => {
-                    const pl = playlist[idx];
+                    const pl = playlist.find((p) => p.id === idx.id);
                     if (pl) {
                       return (
                         <div key={pl.id}>
