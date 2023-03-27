47f3d500cc7e000d476f65278ee5941865e8f434
hajiung25 <hajiung25@gmail.com>
2023-03-27 17:56:56 +0900

    #101 feat: sever 쪽 폴더 삭제

--- server/src/main/java/com/main/server/chat/repository/ChatroomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.main.server.chat.repository;
-
-import com.main.server.chat.entity.Chatroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
-}
